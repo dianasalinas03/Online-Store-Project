8d532b95d230c78b09e06d50161f6cc1e446e6d8
dianasalinas03 <admin@Dianas-MacBook.local>
2025-05-07 23:53:03 -0600

    Primer commit

---
+++ admin/config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+/*Parametros para configuración*/
+
+$path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+$basePath = dirname($path, 2);
+
+require_once $basePath . '/config/database.php';
+
+// Sesión para panel de administración
+session_name('admin_session');
+session_start();
+
+/*URL de la tienda*/
+define('SITE_URL', 'http://localhost/OnlineMediaStore/');
+
+
+/*URL del panel de administración*/
+define('ADMIN_URL', SITE_URL . 'admin/');
+
+/*Clave o contraseña para cifrado*/
+define("KEY_CIFRADO", "ABCD.1234-");
+
+/**
+ * Metodo de cifrado OpenSSL.
+ * https://www.php.net/manual/es/function.openssl-get-cipher-methods.php
+ */
+define("METODO_CIFRADO", "aes-128-cbc");
